f93b8e8cf8235a5bdf14f0b4c2cc39cb77634f68
Lev <4ebotarenko.lev@gmail.com>
2025-06-19 22:53:10 +0200

    Delete fr directory

--- fr/myscripts.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', handleScroll);
-const header = document.querySelector('header');
-function handleScroll() {
-    if (window.scrollY > 75) {
-    header.classList.add('shrunk');
-    } else {
-    header.classList.remove('shrunk');
-    }
-}
